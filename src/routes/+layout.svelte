93b136c94be66b76c4131fc3dc69a4024f81c286
peterpan0225 <wasyl.0225@gmail.com>
2023-02-27 08:05:31 +0200

    nabvar changed

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "../app.css";
+  import Nabvar from "../lib/nabvar/Nabvar.svelte";
+
+  let y : number;
+  $: scrolled = (y > 50) ? true : false;
+  
+</script>
+
+<Nabvar scrolled = {scrolled}></Nabvar>
+<slot />
+
+<svelte:window bind:scrollY={y} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit Test</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import startImg from "../lib/images/start-img.png";
+</script>
+
+<section class="w-full">
+  <div class="w-full relative h-[100vh]">
+    <img class="w-full h-full object-cover" src={startImg} alt=""/>
+    <div class="absolute top-[30vh] w-full items-center">
+      <div class="w-full items-center text-center lg:px-46 md:px-20 sm:px-5">
+        <h1 class="lg:text-[64px] md:text-[32px] xm:text-[24px] text-white font-bold">Discover Your Life, Travel Where You Want</h1>
+        <p class="lg:text-[24px] md:text-[20px] xm:text-[14px] text-white mt-10">Would you explore natur paradise in the world, let’s find the best destination in world with us.</p>
+      </div>
+      
+    </div>
+  </div>
+</section>
+
+<section class="w-full">
+  <div class="w-full flex flex-wrap flex-row">
+    <div class="w-full px-4 py-4">
+      <h1 class="lg:text-[48px] sm:text-[24px] text-[20px] font-semibold text-gray-800 text-center">Special Upcoming Offers</h1>
+    </div>
+    <div class="w-full">
+      
+    </div>
+  </div>
+</section>
+
