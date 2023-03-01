0108e791952e191d13a872607afb5fa13799b6b9
peterpan0225 <wasyl.0225@gmail.com>
2023-03-01 11:04:10 +0200

    changed the CardOffer

--- src/lib/card/CardOffer.svelte
+++ src/lib/card/CardOffer.svelte
@@ -10,10 +10,10 @@
 	<div class="w-full rounded-3xl px-10 py-10 bg-white">
 		<div class="w-full aspect-[1.2] ">
 			<a href="#pa">
-				<img class="w-full h-full rounded-3xl" src={img_name} alt="" />
+				<img class="w-full h-full rounded-3xl ease-in duration-300 hover:scale-105" src={img_name} alt="" />
 			</a>
 		</div>
-		<div class="px-3 mt-5">
+		<div class="px-3 mt-10">
 			<div>
 				<span
 					class="px-5 py-1 bg-[#FCDDEC] text-[#F178B6] rounded-full text-[16px] font-medium mr-5"
@@ -26,7 +26,7 @@
 				<span class="text-[20px] font-medium text-gray-500"
 					><strong class="text-gray-800">{travel_price}$</strong>/person</span
 				>
-				<button class="px-4 py-2 bg-[#F2F6FF] text-[#3E86F5] rounded-sm font-medium"
+				<button class="px-4 py-2 bg-[#F2F6FF] text-[#3E86F5] rounded-lg font-medium"
 					>Book Now</button
 				>
 			</div>
