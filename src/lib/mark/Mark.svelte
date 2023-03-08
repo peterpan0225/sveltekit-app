8a8535b134bfc1a8bef85afc637e37869e4ce46d
peterpan0225 <wasyl.0225@gmail.com>
2023-03-08 18:49:24 +0200

    added mark component

---
+++ src/lib/mark/Mark.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+export let mark: string = ""
+export let description: string = ""
+
+</script>
+
+<div class="w-full items-center p-5">
+  <h1>{mark}</h1>
+  <p>{description}</p>
+</div>
